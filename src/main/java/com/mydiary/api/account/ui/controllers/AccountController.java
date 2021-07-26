e1936fbbc0022734e2f0239cc17198f367bfa2de
naziaq <79763949+naziaq@users.noreply.github.com>
2021-07-26 17:51:51 +0530

    Modified comment!!

--- src/main/java/com/mydiary/api/account/ui/controllers/AccountController.java
+++ src/main/java/com/mydiary/api/account/ui/controllers/AccountController.java
@@ -15,7 +15,7 @@ public class AccountController {
 	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working on port.."+ env.getProperty("local.server.port");
+		return "Working on port!!!.."+ env.getProperty("local.server.port");
 	}
 
 }
